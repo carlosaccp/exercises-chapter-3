772ffaf8c2325f0c7bae54a9d55928fefa57f8b1
Carlos Perello <cac19@ic.ac.uk>
2022-01-24 23:39:46 +0000

    Make ex 3.3.1 work

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -1,4 +1,4 @@
-from numbers import Number
+from numbers import Number, Integral
 
 
 class Polynomial:
@@ -50,3 +50,62 @@ class Polynomial:
 
     def __radd__(self, other):
         return self + other
+    
+    def __sub__(self, other):
+        if isinstance(other, Polynomial):
+            common = min(self.degree(), other.degree()) + 1
+            coefs = tuple(a - b for a, b in zip(self.coefficients,
+                                                other.coefficients))
+            coefs += self.coefficients[common:] + tuple(-i for i in other.coefficients[common:])
+
+            return Polynomial(coefs)
+
+        elif isinstance(other, Number):
+            tuple_first = (self.coefficients[0] - other,)
+            return Polynomial(tuple_first + self.coefficients[1:])
+        
+        else:
+            return NotImplemented
+
+    def __rsub__(self, other):
+        coefs = tuple(-i for i in (self - other).coefficients)
+        return Polynomial(coefs)
+    
+    def __mul__(self, other):
+        if isinstance(other, Polynomial):
+            new_deg = self.degree() + other.degree()
+            coefs = [0] * new_deg
+            for i in range(0, self.degree()+1):
+                for j in range(0, other.degree()+1):
+                    coefs[i+j] == self.coefficients[i]*other.coefficients[j]
+            
+            return Polynomial(coefs)
+
+        elif isinstance(other, Number):
+            coefs = (other * i for i in self.coefficients)
+            return Polynomial(coefs)
+
+        else:
+            return NotImplemented
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __pow__(self, other):
+        if isinstance(other, Integral):
+            temp = Polynomial(self.coefficients)
+            for i in range(0, other):
+                 self = self*temp
+            return self
+        else:
+            return NotImplemented
+
+    def __call__(self, other):
+        if isinstance(other, Number):
+            total = 0
+            for i in range(0, self.degree()+1):
+                total += self.coefficients[i]*(other**i)
+            return total
+
+        else:
+            return NotImplemented
